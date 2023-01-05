5f23bd35e486340586649400883085d8b7ac78ce
Jesse Carson <jmcarson222@gmail.com>
2023-01-05 09:14:14 -0600

    HTML and logic
    Added some classes/id's to the HTML and intalled the logic

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//The colors we'll use to cycle through on this page
+const colors = ["green", "red", "yellow", "dodgerblue", "rgba(133,122,200)", "#f15025"];
+//btn adopts the "btn" id,, color adopts the ".color" class
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+//add an eventListener to btn on click
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener('click', function() {
+    let hexColor= "#";
+    //the for loop will run 6 times because we need 6 random values from the hex array
+    //to add to the hexColor variable.
+    for (let i=0; i<6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}  
